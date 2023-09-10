b8317effcf11d2dc1ebc088997f9fde36dd59d7b
nileshjanakwade <nileshjankwade2@gmail.com>
2023-09-10 21:22:56 +0530

    first commit

---
+++ src/main/java/com/example/demo/SbAppEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbAppEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbAppEngineApplication.class, args);
+	}
+
+}
